9d74753af09ed815f9727cc80c7a702a17663001
MARIA JOSE CARVAJAL SIFONTES <02319799249@senacrs.edu.br>
2026-03-23 17:14:11 -0300

    Operadores

---
+++ Operadores/OperadoresAritmeticos/Conteudo.js
@@ -0,0 +1,86 @@
+//============================================================
+// SOMA
+//============================================================
+const primeiroValor = 10
+const segundoValor = 10
+
+const resultado = primeiroValor + segundoValor
+
+console.log(resultado) //20
+
+//============================================================
+// SUBTRAÇÃO
+//============================================================
+const primeiroValor2 = 10
+const segundoValor2 = 10
+
+const resultado2 = primeiroValor2 - segundoValor2
+
+console.log(resultado2) //0
+
+//============================================================
+// MULTIPLICAÇÃO
+//============================================================
+const primeiroValor3 = 10
+const segundoValor3 = 10        
+
+const resultado3 = primeiroValor3 * segundoValor3
+
+console.log(resultado3) //100
+
+//============================================================
+// DIVISÃO
+//============================================================
+const primeiroValor4 = 10
+const segundoValor4 = 10    
+
+const resultado4 = primeiroValor4 / segundoValor4
+
+console.log(resultado4) //1
+
+
+/* COMPARADORES
+- São eles:
+- Iguald ===
+- Diferente !==
+- Maior e maior igual > e >=
+- Menor e menor ou igual a < e <=
+*/
+
+"1" === "2" //false, são diferentes
+"2" === "2" //true, são iguais
+2 === "2" //false, tipo diferente
+
+const condicao = 1 === 2
+// o valor que sai da comparação
+// pode ser armazenado em uma variável
+
+
+
+"1" !== "2" //true, são diferentes
+"2" !== "2" //false, são iguais
+2 !== "2" //true, tipo diferente
+
+const condicao2 = 1 !== 2 // true
+
+
+
+1 > 2 //false, porque 1 é menor que 2
+2 > 2 //false, porque 2 é igual a 2
+3 > 2 //true, porque 3 é maior que 2
+
+1 >= 2 //false, porque 1 é menor que 2
+2 >= 2 //true, porque 2 é igual a 2
+3 >= 2 //true, porque 3 é maior que 2
+
+
+
+1 < 2 //true, porque 1 é menor que 2
+2 < 2 //false, porque 2 é igual a 2
+3 < 2 //false, porque 3 é maior que 2
+
+1 <= 2 //true, porque 1 é menor que 2
+2 <= 2 //true, porque 2 é igual a 2
+3 <= 2 //false, porque 3 é maior que 2
+
+
---
+++ Operadores/OperadoresAritmeticos/EX3.js
@@ -0,0 +1,6 @@
+const fone = 120
+const parcela = 3    
+
+const resultado4 = fone / parcela
+
+console.log(resultado4) //40
